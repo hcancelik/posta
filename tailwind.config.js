97d3051fffb15f137153fd2acbfbabb5867541dc
hcancelik <celik@me.com>
2021-01-23 17:39:40 +0300

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    "./public/**/*.html",
+    "./src/**/*.html",
+    "./src/**/*.vue",
+    "./src/**/*.js",
+  ],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
