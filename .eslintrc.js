2cb83dc499cfd1c6a9291491695b69a6db011abd
hcancelik <celik@me.com>
2021-01-23 15:27:05 +0300

    Add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:vue/vue3-essential',
+    'prettier/vue',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    "import/no-unresolved": "error",
+  },
+};
