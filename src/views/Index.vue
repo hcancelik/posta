d228749820fb5b71139eeac3230aadc1ff458a37
Can Celik <celik@me.com>
2021-01-27 23:57:00 +0300

    More layout changes and context menu for mailboxes

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,10 +7,11 @@
         <template v-for="mailbox in mailboxes" :key="mailbox.id">
           <router-link
             :to="{ name: 'mailbox', params: { mailbox: mailbox.id } }"
+            @contextmenu="openContextMail(mailbox.id)"
+            :id="mailbox.id"
           >
             <li
               class="w-full px-3 py-4 items-center text-gray-600 dark:text-gray-300 flex bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer transform active:translate-y-0.5 border-b border-gray-100 dark:border-gray-700"
-              @click="selectMailBox(mailbox)"
             >
               <div class="w-10/12 truncate pr-2">
                 {{ mailbox.name }}
@@ -49,35 +50,7 @@
     >
       <template v-if="mailboxes.length > 0">No Mailbox Selected</template>
       <template v-else>
-        <div class="text-gray-400 text-3xl">
-          <div>Setup your application to send emails to {{ appName }}.</div>
-          <div class="m-8 flex justify-center">
-            <router-link :to="{ name: 'settings' }">
-              <button
-                class="rounded bg-gray-700 hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-900 text-lg text-white dark:text-gray-400 px-4 py-2 shadow transform active:translate-y-0.5 flex items-center space-x-2 outline-none focus:outline-none active:outline-none"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="feather feather-settings"
-                >
-                  <circle cx="12" cy="12" r="3"></circle>
-                  <path
-                    d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"
-                  ></path>
-                </svg>
-                <span>View Settings</span>
-              </button>
-            </router-link>
-          </div>
-        </div>
+        <NoMailbox />
       </template>
     </div>
   </Inbox>
@@ -85,10 +58,12 @@
 
 <script>
 import Inbox from "@/views/template/Inbox";
+import { remote } from "electron";
+import NoMailbox from "@/views/components/NoMailbox";
 
 export default {
   name: "Index",
-  components: { Inbox },
+  components: { NoMailbox, Inbox },
   data() {
     return {
       appName: process.env.VUE_APP_NAME,
@@ -110,11 +85,36 @@ export default {
           emailCount: 125,
         },
       ],
+      mailboxContextMenu: null,
+      selectedMailboxIdForContextMenu: null,
     };
   },
+  created() {
+    const menu = new remote.Menu();
+    const that = this;
+
+    menu.append(
+      new remote.MenuItem({
+        label: "Delete Mailbox",
+        async click() {
+          await that.deleteMailbox();
+        },
+      })
+    );
+
+    this.mailboxContextMenu = menu;
+  },
   methods: {
-    selectMailBox(mailbox) {
-      this.selectedMailbox = mailbox;
+    openContextMail(mailboxId) {
+      this.selectedMailboxIdForContextMenu = mailboxId;
+
+      this.mailboxContextMenu.popup({ window: remote.getCurrentWindow() });
+    },
+    async deleteMailbox() {
+      // TODO: Delete mailbox
+      console.log(`Deleting ${this.selectedMailboxIdForContextMenu}`);
+
+      this.selectedMailboxIdForContextMenu = null;
     },
   },
 };
---
+++ src/views/components/NoMailbox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="text-gray-400 text-3xl">
+    <div>Setup your application to send emails to {{ appName }}.</div>
+    <div class="m-8 flex justify-center">
+      <router-link :to="{ name: 'settings' }">
+        <button
+          class="rounded bg-gray-700 hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-900 text-lg text-white dark:text-gray-400 px-4 py-2 shadow transform active:translate-y-0.5 flex items-center space-x-2 outline-none focus:outline-none active:outline-none"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-settings"
+          >
+            <circle cx="12" cy="12" r="3"></circle>
+            <path
+              d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"
+            ></path>
+          </svg>
+          <span>View Settings</span>
+        </button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoMailbox",
+};
+</script>
