34b2ea53f18d9b1553def5d6f0a80129bbb1bdcd
hcancelik <celik@me.com>
2021-01-31 01:50:41 +0300

    Add svg components

---
+++ src/views/components/icons/ChevronLeftIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-chevron-left"
+    >
+      <polyline points="15 18 9 12 15 6"></polyline>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChevronLeftIcon",
+};
+</script>
