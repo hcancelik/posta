4bec4bcdbf2cec5307cc9101bcc3c57307bf22e4
Can Celik <celik@me.com>
2021-02-06 17:22:39 +0300

    Wip tests

--- tests/email.test.js
+++ tests/email.test.js
@@ -5,7 +5,7 @@ async function main() {
     host: "127.0.0.1",
     port: 2525,
     auth: {
-      user: "Test Mailbox 4",
+      user: "Test Mailbox 2",
       pass: " ",
     },
   });
@@ -17,8 +17,26 @@ async function main() {
     bcc: "abc@test.com",
     replyTo: "reply@to.com",
     subject: "Hello ✔",
-    // text: "Hello world?",
+    text: "Hello world?",
     html: "<b>Hello world?</b>",
+    attachments: [
+      {
+        filename: "Test Filename.pdf",
+        path: `${__dirname}/sample.pdf`,
+      },
+      {
+        filename: "Sample Report.xlsx",
+        path: `${__dirname}/sample.xlsx`,
+      },
+      {
+        filename: "This is a very long test filename.pdf",
+        path: `${__dirname}/sample.pdf`,
+      },
+      {
+        filename: "Love is in the air dot com.png",
+        path: `${__dirname}/sample.png`,
+      },
+    ],
   });
 }
 
